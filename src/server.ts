f511996f0205341466902b1489850b8a652c31ee
Orubebe Tamarah <paulorubebe@yahoo.com>
2023-02-16 03:20:50 +0100

    resolve mongoose error, and edit server.ts file

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
-require("dotenv").config();
+
 import http from 'http';
 import app from './app';
-import mongoObj from './services/mongo';
+import mongoObj, {MONGO_URL} from './services/mongo';
 
 
 const server = http.createServer(app);
 
 const PORT = process.env.PORT || 3000
-const MONGO_URL = process.env.MONGO_URL as string;
 
 async function startServer(MONGO_URL: string) {
     await mongoObj.connect(MONGO_URL);
