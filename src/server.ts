e9e772b5f72cf9bd8017b15139e87f4c544d27d7
Orubebe Tamarah <paulorubebe@yahoo.com>
2023-02-16 03:02:30 +0100

    resolve mongoose error

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 require("dotenv").config();
 import http from 'http';
 import app from './app';
-import mongoObj, { MONGO_URL} from './services/mongo';
+import mongoObj from './services/mongo';
 
 
 const server = http.createServer(app);
 
 const PORT = process.env.PORT || 3000
+const MONGO_URL = process.env.MONGO_URL as string;
 
 async function startServer(MONGO_URL: string) {
     await mongoObj.connect(MONGO_URL);
