44cd0a0dc192bbf5bcd69ccf9ec6862fb04faa26
Orubebe Tamarah <paulorubebe@yahoo.com>
2023-02-16 02:56:29 +0100

    Edit mongoose error in server.js

--- src/server.ts
+++ src/server.ts
@@ -8,9 +8,9 @@ const server = http.createServer(app);
 
 const PORT = process.env.PORT || 3000
 
-async function startServer() {
+async function startServer(MONGO_URL: string) {
     await mongoObj.connect(MONGO_URL);
     server.listen(PORT, () => console.log(`http server started on port ${PORT}`));
 }
 
-startServer();
+startServer(MONGO_URL);
