47861e30a947405e799df3b7a75ab9da52347a31
Orubebe Tamarah <paulorubebe@yahoo.com>
2023-02-17 01:09:56 +0100

    Add cors and handler for invalid urls

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,20 @@
+const cors = require('cors');
 import express, { Application, Request, Response } from 'express';
 import productsRouter from './products.routes';
 
 const app: Application = express();
 app.use(express.json());
 
+// I am choosing to allow requests from all origins for the purpose of this assessment.
+// It is normally better, security wise, to allow a whitelist of origins. 
+app.use(cors())
 
 app.get("/", (req: Request, res: Response): void => {
     res.send({message: "Hello there, this is my entry for the mainstack Backend Developer application."});
 })
 
 app.use('/products', productsRouter);
-
+app.use('*', (req: Request, res: Response): void => {
+    res.status(404).json({"message": "There might be a problem with your url as the page you are looking for is not found"})
+})
 export default app;
